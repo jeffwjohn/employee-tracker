b26dfe3e2e4340ee9b4963a47ec50adda159ca14
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-13 15:30:19 -0500

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
+
+CREATE TABLE department
+(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role
+(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee
+(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+)
